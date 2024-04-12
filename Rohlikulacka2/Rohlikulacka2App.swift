03b4083185faf36cccf37aecd0f3d66c52817520
GosutoDev <duchoslav.tomas@gmail.com>
2024-04-12 10:09:42 +0200

    Initial Commit

---
+++ Rohlikulacka2/Rohlikulacka2App.swift
@@ -0,0 +1,17 @@
+//
+//  Rohlikulacka2App.swift
+//  Rohlikulacka2
+//
+//  Created by Tomáš Duchoslav on 12.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct Rohlikulacka2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
